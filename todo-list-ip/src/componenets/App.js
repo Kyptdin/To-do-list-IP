c188551f3f321d070a53b44fe71b2d5935e9f86d
Kyptdin <isaacestrella12@gmail.com>
2023-06-22 15:27:20 -0400

    Created the nav and app compoenent
    Created the componenets for the nav and the app componenet

--- todo-list-ip/src/componenets/App.js
+++ todo-list-ip/src/componenets/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
+import Navbar from "./Navbar";
 export default function App() {
   return <Logo />;
 }
@@ -8,7 +9,7 @@ function Logo() {
   return (
     <React.Fragment>
       <div className="app">
-        <div className="logo">📝</div>
+        <Navbar />
       </div>
     </React.Fragment>
   );
---
+++ todo-list-ip/src/componenets/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Navbar() {
+  return (
+    <React.Fragment>
+      <div className="logo">📝</div>
+      <nav>hi there</nav>;
+    </React.Fragment>
+  );
+}
